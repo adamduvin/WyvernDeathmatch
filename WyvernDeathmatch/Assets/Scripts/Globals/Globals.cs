294a2cfce34981fbf915060c5763da12a5c07c82
adamduvin <axd3010@g.rit.edu>
2020-05-26 20:34:07 -0400

    Basic movement and shooting mechanics implemented.

---
+++ WyvernDeathmatch/Assets/Scripts/Globals/Globals.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Globals
+{
+    public const float delta = 0.00001f;
+
+    public enum DamageType
+    {
+        Bullet,
+        Fire,
+        Ice,
+        Beam        // Need a better name, but I don't want "light"
+    }
+}
---
+++ WyvernDeathmatch/Assets/Scripts/Projectile.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Globals;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    private int impactDamage;
+    [SerializeField]
+    private float areaOfEffectDamage;
+    [SerializeField]
+    private float areaOfEffectRange;
+    [SerializeField]
+    private DamageType damageType;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        if (impactDamage <= 0)
+        {
+            impactDamage = 1;
+        }
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Player":
+                other.GetComponent<PlayerCore>().TakeDamage(impactDamage, damageType);
+                break;
+            default:
+                break;
+        }
+
+        createAreaOfEffect();
+        Destroy(gameObject);
+    }
+
+    public void createAreaOfEffect()
+    {
+        if(areaOfEffectRange >= delta)
+        {
+            // Create AOE
+        }
+    }
+}
